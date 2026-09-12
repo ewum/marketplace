01bc663b16da05549fef62c2c52a8a249483ee11
ewum <alanwvv@gmail.com>
2026-09-12 19:56:20 -0300

    feat: add ask question route

--- routes/questions.js
+++ routes/questions.js
@@ -15,17 +15,31 @@ router.get('/:product_id', (req, res) => {
     )
 })
 
-router.patch('/:question_id', authMiddleware, (req, res) => {
+router.post('/:product_id/ask', (req, res) => {
+    const {product_id} = req.params;
+    const {askwer_id, question} = req.body;
+    db.query(
+        `INSERT INTO product_questions(product_id, asker_id, question)
+        VALUES (?, ?, ?)`,
+        [product_id, askwer_id, question],
+        (err, results) => {
+            if (err) return res.status(500).json({error: err.message});
+            res.status(201).json({message: 'question created successfully'});
+        }
+    )
+})
+
+router.patch('/:question_id/answer', authMiddleware, (req, res) => {
     const {question_id} = req.params;
     db.query(
         `UPDATE user_reviews
-        SET answer = ?, answered_at = NOW()
+        SET answer = ?
         WHERE id = ? AND seller_id = ?`,
         [req.body.answer, question_id, req.user.id],
         (err, results) => {
             if (err) return res.status(500).json({error: err.message});
             if (results.affectedRows === 0) return res.status(404).json({error: 'review not found'});
-            res.json(results);
+            res.status(200).json({message: 'question answered successfully'});
         }
     )
 })
