3fe81b11b4e10aae9f334b997852ff5607186e32
ewum <alanwvv@gmail.com>
2026-09-12 22:00:54 -0300

    style: fix indentation

--- routes/products.js
+++ routes/products.js
@@ -18,7 +18,8 @@ router.get('/:product_id', (req, res) => {
             if (err) return res.status(500).json({error: err.message});
             if (results.length == 0) return res.status(404).json({error: 'product not found'});
             res.json(results[0]);
-    });
+        }
+    );
 });
 
 module.exports = router;
