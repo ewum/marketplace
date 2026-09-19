a39d1185ca80cf02e592a46582cb7589d8212453
ewum <alanwvv@gmail.com>
2026-09-19 02:30:02 -0300

    feat: add cookie-parser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cookie_parser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use(cookie_parser());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/products', require('./routes/products'));
--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function authMiddleware(req, res, next) {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
+    const authHeader = req.cookies.token;
     if (!token) {
         return res.status(401).json({error: 'no token provided'});
     }
