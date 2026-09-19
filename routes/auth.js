d09f81aa5920ce80f50afbc9467b588230dcdd8f
ewum <alanwvv@gmail.com>
2026-09-19 03:19:31 -0300

    fix: syntax error

--- routes/auth.js
+++ routes/auth.js
@@ -56,7 +56,7 @@ router.post('/login', (req, res ) => {
     });
 });
 
-router.post('/logout', {req, res} => {
+router.post('/logout', (req, res) => {
     req.clearCookie('token');
     res.status(200).json({message: 'logged out successfully'});
 });
