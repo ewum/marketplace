4f6c0df05c52d6df8f47cb069930afe137d9ef8d
ewum <alanwvv@gmail.com>
2026-09-19 23:21:11 -0300

    fix: define authMiddleware

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const authMiddleware = require('../middlewares/authmiddleware');
 
 router.get('/', (req, res) => {
     db.query('SELECT * FROM products', (err, results) => {
