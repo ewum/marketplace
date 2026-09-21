4b216e3de6b6cbadb17f99ff31a9f5b3ed7211ba
ewum <alanwvv@gmail.com>
2026-09-21 20:13:29 -0300

    style: fix indentation

--- routes/products.js
+++ routes/products.js
@@ -36,9 +36,8 @@ router.get('/user/user_id', (req, res) => {
             if (results.length == 0) return res.status(404).json({error: 'no products found'});
             res.json(results);
         }
-    
     );
-})
+});
 
 router.post('/create', authMiddleware, (req, res) => {
     const {name, description, price, stock} = req.body;
