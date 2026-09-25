54e4f3e1fe1265ee16e2bd37bb2a2bbf0653ea2d
ewum <alanwvv@gmail.com>
2026-09-24 23:49:29 -0300

    fix unexpected string

--- routes/products.js
+++ routes/products.js
@@ -25,7 +25,7 @@ router.get('/', (req, res) => {
     db.query(sql, params, (err, results) => {
         if (err) {
             console.error(err);
-            return res.status(500).json({error: 'internal server error'})''
+            return res.status(500).json({error: 'internal server error'});
         }
         res.json(results);
     });
